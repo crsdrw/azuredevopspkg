be4bb1568ec0b268c0e9808f8f9d02b704c2f35f
Chris Drew <chris@cdrew.co.uk>
2018-10-27 23:58:19 +0100

    Parse path and fill html template

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,14 +1,67 @@
 package handlers
 
 import (
-	"fmt"
+	"errors"
+	"html/template"
 	"net/http"
+	"strings"
 )
 
+var errIncompletePath = errors.New("incomplete path")
+
+type parts struct {
+	org, proj, repo, subs string
+}
+
+func extractRepoPath(path string) (parts, error) {
+	p := strings.SplitN(path[1:], "/", 4)
+	if len(p) < 3 {
+		return parts{}, errIncompletePath
+	}
+	ps := parts{
+		org:  p[0],
+		proj: p[1],
+		repo: p[2],
+	}
+	if len(p) > 3 {
+		ps.subs = p[3]
+	}
+	return ps, nil
+}
+
+var tmpl = template.Must(template.New("vanity").Parse(`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<meta name="go-import" content="{{.Host}}/{{.Org}}/{{.Proj}}/{{.Repo}} git https://dev.azure.com/{{.Org}}/{{.Proj}}/_git/{{.Repo}}">
+	<meta http-equiv="refresh" content="0; url=https://dev.azure.com/{{.Org}}/{{.Proj}}/_git/{{.Repo}}">	
+    <title>Vanity URL for Go package hosted on Azure DevOps git repository</title>
+  </head>
+  <body>
+    Please <a href="https://dev.azure.com/{{.Org}}/{{.Proj}}/_git/{{.Repo}}"> the package on Azure DevOps</a>.
+  </body>
+</html>
+`))
+
 func Index(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
+	p, err := extractRepoPath(r.URL.Path)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprint(w, "Hello, World!")
+	w.Header().Set("Cache-Control", "public, max-age=86400") // Cache for 24 hours.
+	err = tmpl.Execute(w, struct {
+		Host string
+		Org  string
+		Proj string
+		Repo string
+	}{
+		Host: r.Host,
+		Org:  p.org,
+		Proj: p.proj,
+		Repo: p.repo,
+	})
+	if err != nil {
+		http.Error(w, "render error", http.StatusInternalServerError)
+	}
 }
