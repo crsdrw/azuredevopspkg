cc26878af12b3b788f9179518262586a4f98773e
Chris Drew <chris@cdrew.co.uk>
2018-11-01 20:42:54 +0000

    Fix tests

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -26,11 +26,11 @@ func TestParsePath(t *testing.T) {
 		parts parts
 	}{
 		{"/myorg/myproj/myrepo", parts{org: "myorg", proj: "myproj", repo: "myrepo"}},
-		{"/myorg/myproj/myrepo/sub1", parts{org: "myorg", proj: "myproj", repo: "myrepo", subs: "sub1"}},
-		{"/myorg/myproj/myrepo/sub1/", parts{org: "myorg", proj: "myproj", repo: "myrepo", subs: "sub1/"}},
+		{"/myorg/myproj/myrepo/sub1", parts{org: "myorg", proj: "myproj", repo: "myrepo"}},
+		{"/myorg/myproj/myrepo/sub1/", parts{org: "myorg", proj: "myproj", repo: "myrepo"}},
 		{"/myorg/myproj/myrepo.git", parts{org: "myorg", proj: "myproj", repo: "myrepo.git"}},
-		{"/myorg/myproj/myrepo.git/sub1", parts{org: "myorg", proj: "myproj", repo: "myrepo.git", subs: "sub1"}},
-		{"/myorg/myproj/myrepo/sub1/sub2", parts{org: "myorg", proj: "myproj", repo: "myrepo", subs: "sub1/sub2"}},
+		{"/myorg/myproj/myrepo.git/sub1", parts{org: "myorg", proj: "myproj", repo: "myrepo.git"}},
+		{"/myorg/myproj/myrepo/sub1/sub2", parts{org: "myorg", proj: "myproj", repo: "myrepo"}},
 	}
 	for _, tc := range tcs {
 		t.Run(tc.path, func(t *testing.T) {
